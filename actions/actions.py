de5787a7ee0d6fe0e1ab1d6ecf2ec739b81d6065
Vuong Nguyen <97358392+vuongnguyendev2001@users.noreply.github.com>
2023-12-17 11:14:29 +0700

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -8,21 +8,18 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 #
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+
+class ActionHandleFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Đây là nơi bạn có thể định nghĩa hành động xử lý khi chatbot không hiểu
+        dispatcher.utter_message("Xin lỗi, tôi không hiểu yêu cầu của bạn. Bạn có thể đặt câu hỏi cụ thể hơn hoặc yêu cầu giúp đỡ về một chủ đề cụ thể nào đó.")
+        return []
